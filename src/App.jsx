637cbc4e3d931ebcefca92a67c9ab59f1a40bad0
r11725051 <r11725051@ntu.edu.tw>
2023-04-11 00:08:00 +0800

    f-merge

--- src/App.jsx
+++ src/App.jsx
@@ -11,58 +11,7 @@ import "./fonts/Bauhaus.ttf";
 import Router from "./Router.jsx";
 
 function App() {
-  return <Router/>
-  // const [flows, setFlows] = useState(1);
-  // const [login, setLogin] = useState(false);
-  // return (
-  //   <div className="App">
-  //     {!login && <Login setLogin={setLogin} />}
-  //     {login && (
-  //       <>
-  //         <div className="App-sidebar">
-  //           <Sidebar flows={flows} setFlows={setFlows} />
-  //         </div>
-  //         <div className="App-tab">
-  //           <Tab flows={flows} setFlows={setFlows} />
-  //         </div>
-  //       </>
-  //     )}
-  //     {/* <div className="FlowEditor">
-  //       <FlowWithProvider/>
-  //     </div> */}
-  //     {/* <Editor /> */}
-  //   </div>
-  // );
-
-
-    <div className="App">
-      {!login && <Login setLogin={setLogin} />}
-      {login && (
-        <>
-          <div className="App-sidebar">
-            <Sidebar
-              setFlows={setFlows}
-              setLibrary={setLibrary}
-              setCalendar={setCalendar}
-              setSetting={setSetting}
-            />
-          </div>
-          <div className="App-tab">
-            <Tab
-              flows={flows}
-              library={library}
-              calendar={calendar}
-              setting={setting}
-            />
-          </div>
-        </>
-      )}
-      {/* <div className="FlowEditor">
-        <FlowWithProvider />
-      </div> */}
-      {/* <Editor /> */}
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
