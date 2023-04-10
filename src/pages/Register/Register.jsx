ac508c2fc67510ee69f8d0b03b6e4c9f45b28d35
楊于晨 <r10323003@ntu.edu.tw>
2023-04-10 18:17:20 +0800

    Register
    Register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,7 +5,7 @@ import TextField from "@mui/material/TextField";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import jwt_decode from "jwt-decode";
-import "./Login.scss";
+import "./Register.scss";
 import instance from "../../api";
 import { SHA256 } from "crypto-js";
 import { useParams } from "../../hooks/useParams";
@@ -24,11 +24,12 @@ const Profile = ({ user }) => {
   );
 };
 
-const Login = () => {
+const Register = () => {
   const [user, setUser] = useState({}); // user 是 google 回傳的 object, 可以拿去 render profile 頁面
-  const divRef = useRef(null);
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
   const { setLogin } = useParams();
   const navigateTo = useNavigate();
 
@@ -42,14 +43,15 @@ const Login = () => {
 
   const handleLogOut = (e) => {
     setUser({});
-    navigateTo("/Register");
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const passwordHashed = SHA256(password).toString();
+    const checkPasswordHashed = SHA256(checkPassword).toString();
     const request = {
       user: {
+        name: name,
         email: email,
         password: passwordHashed,
       },
@@ -64,8 +66,11 @@ const Login = () => {
     //     password: SHA256("112a").toString(),
     //   },
     // };
+    if (passwordHashed !== checkPasswordHashed) {
+      alert("Wrong password");
+    }
     instance
-      .post("/user/login", request)
+      .post("/user/register", request)
       .then((res) => {
         console.log(res.data);
         setLogin(true);
@@ -78,31 +83,6 @@ const Login = () => {
     //
   };
 
-  useEffect(() => {
-    /* global google */
-    if (divRef.current) {
-      google.accounts.id.initialize({
-        client_id: client_id,
-        callback: handleCallbackResponse,
-      });
-
-      google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-        theme: "dark",
-        width: "330",
-      });
-
-      google.accounts.id.prompt();
-    }
-  }, [divRef.current]);
-
-  useEffect(() => {
-    if (Object.keys(user).length === 0) {
-      divRef.current.style.display = "flex";
-    } else {
-      divRef.current.style.display = "none";
-    }
-  }, [user]);
-
   return (
     <div className="login">
       <div className="login-container">
@@ -111,7 +91,7 @@ const Login = () => {
           <h1>NoteFlow</h1>
         </div>
         <div className="info">
-          <h2>Log in</h2>
+          <h2>Register</h2>
           {/* <button onClick={() => handleLogin()}>Login</button> */}
 
           <div className="infoContainer">
@@ -127,11 +107,24 @@ const Login = () => {
                     margin="normal"
                     required
                     fullWidth
+                    id="name"
+                    label="Name"
+                    name="name"
+                    autoComplete="name"
+                    autoFocus
+                    size="small"
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
                     id="email"
                     label="Email Address"
                     name="email"
                     autoComplete="email"
-                    autoFocus
                     size="small"
                     onChange={(e) => {
                       setEmail(e.target.value);
@@ -151,6 +144,20 @@ const Login = () => {
                       setPassword(e.target.value);
                     }}
                   />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Check Password"
+                    type="password"
+                    id="check-password"
+                    autoComplete="current-password"
+                    size="small"
+                    onChange={(e) => {
+                      setCheckPassword(e.target.value);
+                    }}
+                  />
 
                   <Button
                     type="submit"
@@ -159,31 +166,12 @@ const Login = () => {
                     sx={{ mt: 2, mb: 2 }}
                     style={{ backgroundColor: "#0e1111" }}
                   >
-                    Log in
+                    Register
                   </Button>
-                  <div
-                    className="links"
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Link href="#" variant="body2" style={{ color: "#414a4c" }}>
-                      Forgot password?
-                    </Link>
-                    <Link href="#" variant="body2" style={{ color: "#414a4c" }}>
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </div>
                 </Box>
               </>
             )}
           </div>
-          <div className="horizontalLine">
-            <span>OR</span>
-          </div>
-          <div id="signInDiv" ref={divRef}></div>
-          {user && <Profile user={user} />}
 
           {/* {Object.keys(user).length !== 0 && (
             <Button
@@ -199,34 +187,10 @@ const Login = () => {
               Log out
             </Button>
           )} */}
-
-          <div
-            className="links"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              width: "65%",
-            }}
-          >
-            <Link href="#" variant="body2" style={{ color: "#414a4c" }}>
-              Doesn't have an account yet?
-            </Link>
-            <Button
-              type="submit"
-              variant="contained"
-              style={{ backgroundColor: "#0e1111", width: "50%" }}
-              onClick={(e) => {
-                handleLogOut(e);
-              }}
-            >
-              Register
-            </Button>
-          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export { Login, Profile };
+export { Register, Profile };
