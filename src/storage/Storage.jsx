c5f450cdfb8f116ee30b72e8a6c8e22d01f520e3
oohyuti <yuti0622@gmail.com>
2023-04-11 23:03:52 +0800

    first demo version

--- src/storage/Storage.jsx
+++ src/storage/Storage.jsx
@@ -55,31 +55,35 @@ export const useFlowStorage = create((set) => ({
   saveNode: (payload) =>
     set(
       produce((state) => {
+        let findNode = false;
+        let findFlow = false;
         for (let ele in state.flowNodes) {
           if (payload.flow_id === state.flowNodes[ele].id) {
+            findFlow = true;
             for (let el in state.flowNodes[ele].nodes) {
               if (payload.node_id === state.flowNodes[ele].nodes[el].id) {
-                state.flows[ele].nodes[el].data.label = payload.title;
+                findNode = true;
+                console.log(payload.title)
+                // state.flows[ele].nodes[el].data.label = payload.title;
                 state.flowNodes[ele].nodes[el].value = payload.value;
               }
             }
+            if(!findNode){
+              state.flowNodes[ele].nodes.push({id: payload.node_id, value: payload.value})
+            }
           }
         }
+        if(!findFlow){  
+          state.flows.push({id: payload.flow_id, nodes:[{id: payload.node_id, data:{label: payload.title}}]})          
+          state.flowNodes.push({id: payload.flow_id, nodes:[{id: payload.node_id, value: payload.value}]})
+        }
+
       })
     ),
   addFlow: (payload) =>
   set(
     produce((state) => {    
       state.flows.unshift(payload);
-      // for(let ele in state.flows){
-      //   if(payload.id == state.flows[ele].id){
-      //     state.flows[ele].edges = payload.flow.edges;
-      //     state.flows[ele].viewport = payload.flow.viewport;
-      //     state.flows[ele].nodes = payload.flow.nodes;
-      //     state.flows[ele].nextNodeId = payload.nextNodeId;
-      //     state.flows[ele].name = payload.title;
-      //   }
-      // }
     })
   ),
 }));
